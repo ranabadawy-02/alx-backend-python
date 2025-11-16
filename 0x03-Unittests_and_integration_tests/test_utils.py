fe14685b76c0df5df38d75b9749375d352f59173
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-16 18:24:40 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -32,25 +32,3 @@ class TestAccessNestedMap(unittest.TestCase):
             self.assertEqual(error.exception.args[0], path[-1])
 
 
-#!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map"""
-
-import unittest
-from parameterized import parameterized
-from utils import access_nested_map
-
-class TestAccessNestedMap(unittest.TestCase):
-    """Test access_nested_map function"""
-
-    @parameterized.expand([
-        ({}, ("a",), "Key not found: 'a'"),
-        ({"a": 1}, ("a", "b"), "Key not found: 'b'")
-    ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_message):
-        """Test that access_nested_map raises KeyError with correct message"""
-        with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), expected_message)
-
-if __name__ == "__main__":
-    unittest.main()
