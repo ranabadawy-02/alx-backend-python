36b3aa9661447fadbfa93ecbdb241392d20529b1
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-22 13:32:18 +0200

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,6 +2,32 @@ from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from .models import Conversation, Message, User
 from .serializers import ConversationSerializer, MessageSerializer, UserSerializer
+from .permissions import IsParticipantOfConversation
+
+
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsParticipantOfConversation]
+
+    def get_queryset(self):
+        # Only show conversations where the user is a participant
+        return Conversation.objects.filter(participants=self.request.user)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsParticipantOfConversation]
+
+    def get_queryset(self):
+        # Only messages inside conversations the user participates in
+        return Message.objects.filter(conversation__participants=self.request.user)
 
 # -----------------------------
 # Conversation ViewSet
