da62c4fc4a45864b41512c8c4543807963390571
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-22 22:07:29 +0200

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,4 +1,10 @@
 from rest_framework import permissions
+from rest_framework.pagination import PageNumberPagination
+
+class MessagePagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class IsParticipantOfConversation(permissions.BasePermission):
     """
