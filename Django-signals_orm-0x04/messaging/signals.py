5ad7ef4ef75c1abad006c829bad57688ff6962df
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-30 10:57:53 +0200

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -31,3 +31,27 @@ def log_message_edit(sender, instance, **kwargs):
                 message=instance,
                 old_content=old_message.content
             )
+
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Message, Notification, MessageHistory
+
+
+@receiver(post_delete, sender=User)
+def delete_related_data(sender, instance, **kwargs):
+    """
+    Clean up all messages, notifications, and history related to the deleted user.
+    """
+
+    # Delete all messages sent by the user
+    Message.objects.filter(sender=instance).delete()
+
+    # Delete all messages received by the user
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete all notifications for this user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete all message history where the user was the editor
+    MessageHistory.objects.filter(edited_by=instance).delete()
