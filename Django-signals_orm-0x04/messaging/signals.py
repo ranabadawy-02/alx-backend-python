a51fb5fb28800addb90ab7caa14d72bde0790f1a
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-30 10:52:59 +0200

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 
 @receiver(post_save, sender=Message)
@@ -11,3 +11,23 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """
+    Before a message is saved, check if it's being edited.
+    If yes, store the old content in MessageHistory.
+    """
+    if instance.pk:
+        old_message = Message.objects.get(pk=instance.pk)
+
+        if old_message.content != instance.content:
+            # Mark message as edited
+            instance.edited = True
+
+            # Save old content in history
+            MessageHistory.objects.create(
+                message=instance,
+                old_content=old_message.content
+            )
