e6e5d076bf5185c269e833f696b582974c8a55b0
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-11-30 16:52:26 +0200

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from .models import Message
+
+def unread_inbox(request):
+    unread_messages = Message.unread.for_user(request.user)
+    return render(request, "messaging/unread_inbox.html", {"messages": unread_messages})
 
 
 @login_required
